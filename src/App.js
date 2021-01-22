165bf0424807614a654fe39134c80d32647f9d25
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-01-22 15:35:27 -0800

    fixing files and adding example styles

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ import About from './pages/About'
 export default function App() {
   return (
     <Router>
-      <div className="App">
-        <ul>
+      <div>
+        <ul className="menu">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -27,28 +27,20 @@ export default function App() {
             <Link to="/dashboard">Dashboard</Link>
           </li>
         </ul>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
       </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+
     </Router>
   );
 }
