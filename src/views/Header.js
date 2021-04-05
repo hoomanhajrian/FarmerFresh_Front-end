7d5dc657337e32558fb92798d8690cbe45eb439a
juancfsantos <juan.santos@live.com>
2021-04-05 14:04:33 -0700

    fixed apk

--- src/views/Header.js
+++ src/views/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <div className="Header" style={{ backgroundImage: "url(/img/bg_apple.webp)" }}>
         <div className="site-header-text"><h1>A Fresh produce to your doorstep.</h1>
           <p>Browse what you are looking and feel satisfied. Download the app by clicking below:</p>
-          <a href="files/FarmerFreshV2.6.apk" download="Farmerfresh_App"><img src="img/google-play-badge.png" alt="google play button" /></a></div>
+          <a href="files/FarmerFreshV2.9.apk" download="Farmerfresh_App"><img src="img/google-play-badge.png" alt="google play button" /></a></div>
       
         
         <div className="header_social_media ClearClass"> <a href="#"><i className="fab fa-pinterest-p"></i></a>
