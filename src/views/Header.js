5d9037875c81ec93be3a8c41843614b2500a2530
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2022-07-16 23:16:56 -0700

    changed button styling from google to normal button

--- src/views/Header.js
+++ src/views/Header.js
@@ -5,7 +5,8 @@ function Header() {
     <div className="Header" style={{ backgroundImage: "url(/img/bg_apple.webp)" }}>
       <div className="site-header-text"><h1>A Fresh produce to your doorstep.</h1>
         <p>Browse what you are looking and feel satisfied. Download the app by clicking below:</p>
-        <a href="files/FarmerFreshV2.9.apk" download="Farmerfresh_App"><img src="img/google-play-badge.png" alt="google play button" /></a></div>
+        <a href="files/FarmerFreshV2.9.apk" download="Farmerfresh_App" className='download-app-button'>Download App</a>
+      </div>
 
 
       <div className="header_social_media ClearClass"> <a href="#"><i className="fab fa-pinterest-p"></i></a>
@@ -16,4 +17,4 @@ function Header() {
   );
 }
 
-export default Header
+export default Header;
